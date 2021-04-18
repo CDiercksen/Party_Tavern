aacce64f4efa62b1e9dcb1b55a676ea65fa4cbf8
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-04-18 13:08:07 -0400

    updated validations in models

--- app/models/adventurer.rb
+++ app/models/adventurer.rb
@@ -1,3 +1,12 @@
 class Adventurer < ActiveRecord::Base
     belongs_to :user
+
+    validates 
+    :name, 
+    :fantasy_origin, 
+    :class, 
+    :hometown, 
+    :background, 
+    :weapon, 
+    :nemesis,  presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     
-    has_many :Adventurers
+    has_many :adventurers
 
     validates :username, :name, presence: true
     validates :username, uniqueness: true
