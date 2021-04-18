3e85fc60b3fa3b3db9ead6fd03a1e4cdba334f58
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-04-18 12:18:19 -0400

    add password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_030749) do
+ActiveRecord::Schema.define(version: 2021_04_18_161315) do
 
   create_table "Adventurers", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_18_030749) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
+    t.string "password_digest"
   end
 
 end
