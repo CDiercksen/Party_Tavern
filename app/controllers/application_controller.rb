5d5b06f957c9a0240ef1c198be7251ad971397e5
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-04-18 12:12:04 -0400

    enabled sessions in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #required for the browser to keep track of persistent data
+    set :session_secret, "thisisoursessionsecret"
   end
 
   get "/" do
