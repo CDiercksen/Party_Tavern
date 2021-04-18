44ee0e522c105a2f01bf1d42e3e8c197ef7c9e52
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-04-17 23:47:46 -0400

    add seed data for adventurers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,26 @@
 User.destroy_all
-Thought.destroy_all
+Adventurer.destroy_all
 
 3.times do
     User.create(
         name: "",
         username: "",
         password: "password")
+end
+
+5.times do
+    Adventurer.create(
+        name: "",
+        fantasy_origin: "",
+        class: "",
+        hometown: "",
+        background: "",
+        weapon: "",
+        nemesis: "",
+
+        experience_level: [1...20].sample
+        user_id: User.all.sample
+    )
 end
 
 
