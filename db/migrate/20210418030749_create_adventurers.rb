13be27f6be0dbcce3739444d413bbd427df0572b
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-04-17 23:15:49 -0400

    define adventurers table in migration

--- db/migrate/20210418030749_create_adventurers.rb
+++ db/migrate/20210418030749_create_adventurers.rb
@@ -1,4 +1,17 @@
 class CreateAdventurers < ActiveRecord::Migration[5.2]
   def change
+    create_table :Adventurers do |t|
+
+      t.string :name
+      t.string :fantasy_origin
+      t.string :class
+      t.string :hometown
+      t.string :background
+      t.string :weapon
+      t.string :nemesis
+
+      t.integer :experience_level
+      t.integer :user_id
+    end
   end
 end
