92831c7d8df21bdc9fa0aae2f93d78e47fd74836
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-04-18 12:32:21 -0400

    added login? adn current_user helper methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,15 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
+  helpers do #tells sinatra "these should be available everywhere"
 
+    def logged_in?
+      session[:user_id]
+    end
+  
+    def current_user
+     @user ||= User.find_by(id: session[:user_id]) #If we've already done the database lookup, return the @user instance variable OR SET User as found by specific session user_id
+    end
+  
+  end
 end
